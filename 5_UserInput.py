7ebd2c38dcd1f50bd90fa54594d1d76ca6c64ecf
Kevin Richey <kevin.richey@gmail.com>
2014-06-18 16:24:07 -0400

    User input example

---
+++ 5_UserInput.py
@@ -0,0 +1,43 @@
+import pygame
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((800,600))
+screen_rect = screen.get_rect()
+
+#### Game Pieces ############################
+box = pygame.Rect(10, 200, 200, 100)
+box_color = pygame.Color("yellow")
+box_speed = 0
+
+##### Play Loop #############################
+running = True
+while running:
+
+    # Run 60 frames per second
+    clock.tick(60)
+
+    ##### User Input ########################
+    # End the game when player closes the window.
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        running = False
+    elif event.type == pygame.KEYDOWN:
+        box_speed = 3
+    elif event.type == pygame.KEYUP:
+        box_speed = 0
+
+    ##### Game Rules ########################
+    box.centerx += box_speed
+
+    ##### Draw the Screen ###################
+    # Start with a blank screen
+    screen.fill(pygame.Color("black"))
+
+    # Draw game graphics here...
+    pygame.draw.rect(screen, box_color, box)
+
+    # Show the screen. This must come last.
+    pygame.display.flip()
+
+pygame.quit()
+
